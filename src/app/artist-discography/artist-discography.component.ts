f9fb9d24f459115ba97e77facc056add12f54492
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-03-02 11:40:53 -0500

    added html for /artist

--- src/app/artist-discography/artist-discography.component.ts
+++ src/app/artist-discography/artist-discography.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as albumData from '../data/SearchResultsAlbums.json';
+import * as artistData from '../data/SearchResultsArtist.json';
 @Component({
   selector: 'app-artist-discography',
   templateUrl: './artist-discography.component.html',
   styleUrls: ['./artist-discography.component.css']
 })
 export class ArtistDiscographyComponent implements OnInit {
-
+  albums = null;
+  artist = null;
   constructor() { }
 
   ngOnInit(): void {
+    this.albums = albumData.albums.items;
+    this.artist = artistData;
   }
 
 }

2f1a782ac3b4fdde6c32286d1788e5824ec70675
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-03-24 10:47:33 -0400

    artist page now fetches dynamic data

--- src/app/artist-discography/artist-discography.component.ts
+++ src/app/artist-discography/artist-discography.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as albumData from '../data/SearchResultsAlbums.json';
 import * as artistData from '../data/SearchResultsArtist.json';
+import { ActivatedRoute } from '@angular/router';
+import {MusicDataService} from '../music-data.service';
 @Component({
   selector: 'app-artist-discography',
   templateUrl: './artist-discography.component.html',
@@ -9,11 +11,32 @@ import * as artistData from '../data/SearchResultsArtist.json';
 export class ArtistDiscographyComponent implements OnInit {
   albums = null;
   artist = null;
-  constructor() { }
-
+  private id : any;
+  private idSub;
+  private artistSub: any;
+  private albumSub: any;
+  //newArtist = null;
+  //constructor() { }
+  
+  constructor(private route : ActivatedRoute, private data: MusicDataService) {}
   ngOnInit(): void {
-    this.albums = albumData.albums.items;
-    this.artist = (artistData as any).default;
+    this.idSub = this.route.params.subscribe(params => {
+      this.id = params['id']; // (+) converts string 'id' to a number
+      this.albumSub = this.data.getAlbumsByArtistId(this.id).subscribe(data => this.albums = data.items);
+      this.artistSub = this.data.getArtistById(this.id).subscribe(data => this.artist = data);
+   });
+
+
+   
+    //this.artist = (artistData as any).default;
+    //this.albums = albumData.albums.items;
   }
 
+  ngOnDestroy() {
+    this.idSub.unsubscribe();
+    this.artistSub.unsubscribe();
+    this.albumSub.unsubscribe();
+    
+    
+  }
 }
--- src/app/new-releases/new-releases.component.ts
+++ src/app/new-releases/new-releases.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as data from '../data/NewReleasesAlbums.json';
+import {MusicDataService} from '../music-data.service';
+
 @Component({
   selector: 'app-new-releases',
   templateUrl: './new-releases.component.html',
   styleUrls: ['./new-releases.component.css']
 })
-export class NewReleasesComponent implements OnInit {
+export class NewReleasesComponent implements OnInit, OnDestroy {
 
   releases = null;
+  //public releases = null;
+  //constructor(public data : MusicDataService) { }
   constructor() { }
-
   ngOnInit(): void {
     this.releases = data.albums.items;
+  // this.releases = this.data.getNewReleases().subscribe();
+  }
+
+  ngOnDestroy(){
+    //this.releases.unsubscribe();
   }
 
 }

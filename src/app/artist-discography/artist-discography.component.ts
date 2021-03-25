c971488463f39500bd5c1c92f0743758fd6d927a
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-03-25 10:11:46 -0400

    albums component now does not render duplicate data

--- src/app/artist-discography/artist-discography.component.ts
+++ src/app/artist-discography/artist-discography.component.ts
@@ -13,20 +13,27 @@ export class ArtistDiscographyComponent implements OnInit {
   private idSub;
   private artistSub: any;
   private albumSub: any;
-  //newArtist = null;
-  //constructor() { }
-  
+
+  //Function referenced from https://ilikekillnerds.com/2016/05/removing-duplicate-objects-array-property-name-javascript/
+  removeDuplicates(myArr, prop) {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
+
+}
   constructor(private route : ActivatedRoute, private data: MusicDataService) {}
   ngOnInit(): void {
     this.idSub = this.route.params.subscribe(params => {
       this.id = params['id'];
-      this.albumSub = this.data.getAlbumsByArtistId(this.id).subscribe(data => this.albums = data.items);
+      this.albumSub = this.data.getAlbumsByArtistId(this.id).subscribe(data => {
+        this.albums = this.removeDuplicates(data.items,"name");
+      });
       this.artistSub = this.data.getArtistById(this.id).subscribe(data => this.artist = data);
    });
 
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.idSub.unsubscribe();
     this.artistSub.unsubscribe();
     this.albumSub.unsubscribe();

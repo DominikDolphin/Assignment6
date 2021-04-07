4fc3aff1acff9b9086eb77f6ca17b30761f327e6
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-04-07 16:43:37 -0400

    ensured user input for Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../auth.service';
+import { AuthService } from '../auth.service';
 import { NgForm } from '@angular/forms';
 import { Router } from "@angular/router"
 @Component({
@@ -11,28 +11,31 @@ export class LoginComponent implements OnInit {
   //user: {userName: "", password: "", _id: null};]
   user;
   warning: string;
-  loading : boolean = false;
-  constructor(private auth:AuthService, private router:Router) { }
+  loading: boolean = false;
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    this.user = {userName: "", password: "", _id: null};
+    this.user = { userName: "", password: "", _id: null };
   }
 
 
   onSubmit(f: NgForm): void {
-    console.log("click")
-    console.log(this.user);
-    this.auth.login(this.user).subscribe(
-      (success) => {
-        // store the returned token in local storage as 'access_token'
-        localStorage.setItem('access_token',success.token);
-        // redirect to the "vehicles" route
-        this.router.navigate(['/newReleases']);
-      },
-      (err) => {
-        this.warning = err.error.message;
-      }
-    );
 
+    if (this.user.userName != "" && this.user.password != "") {
+      this.loading = true;
+      this.auth.login(this.user).subscribe(
+        (success) => {
+          this.loading = false;
+          localStorage.setItem('access_token', success.token);
+          this.router.navigate(['/newReleases']);
+        },
+        (err) => {
+          this.loading = false;
+          this.warning = err.error.message;
+        }
+      );
+    } else {
+      this.warning = "Please fill in all fields";
+    }
   }
 }
